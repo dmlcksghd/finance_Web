d38f448e710e9cc283b1357abd29be88c7940d68
HongEC <honlucete@naver.com>
2024-09-24 19:07:57 +0900

    views

--- finance_Web/accounts/views.py
+++ finance_Web/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from .forms import SighupForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)    # 회원가입 후 자동 로그인
+            return redirect('dashboard')    # 로그인 후 이동할 페이지
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html', {'form': form})
