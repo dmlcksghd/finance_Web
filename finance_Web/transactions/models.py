e0aac6240fb3f5454e8743863fc1677048fea6ed
HongEC <honlucete@naver.com>
2024-09-24 18:32:00 +0900

    Transaction Model

--- finance_Web/transactions/models.py
+++ finance_Web/transactions/models.py
@@ -1,3 +1,5 @@
+# 사용자가 지출이나 수입을 입력할 때 해당 항목이 어떤 카테고리에 속하는지 분류하기 위해 사용
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,3 +9,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateField()
+    description = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.type} - {delf.amount}"
