c5b64f12178ace9d5bec8019491eec29ffe081b7
HongEC <honlucete@naver.com>
2024-09-24 19:16:10 +0900

    finance_Web urls.py

--- finance_Web/finance_Web/urls.py
+++ finance_Web/finance_Web/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),    # accounts 입의 URL 포함
+    path('', include('transaction.urls')),
 ]
